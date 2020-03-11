59025cff1c4284e02df46d522203982ba9e301a8
zyf1991 <467225809@qq.com>
2020-03-11 21:40:06 +0800

    登录接口用户信息

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    wx.checkSession({
+      success () {
+        console.log('未过期');
+      },
+      fail () {
+        // session_key 已经失效，需要重新执行登录流程
+        // 登录
+          wx.login({
+            success: res => {
+              if(res.code){
+                //console.log(res.code);
+                  var globaldata =this.globalData;
+                  console.log(globaldata);
+                  //发送 res.code 到后台换取 openId, sessionKey, unionId
+                  wx.request({
+                    url: 'http://local.miniadmin.com/api/login',
+                    data: {
+                      code: res.code,
+                      userinfo:globaldata
+                    },
+                    success: function (res) {
+                      console.log(res);
+                    }
+                  })
+              }else{
+                console.log('login error');
+              }
+              
+            }
+          })
       }
     })
+    
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -21,13 +48,14 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              //console.log(res.userInfo);
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
             }
+           
           })
         }
       }
